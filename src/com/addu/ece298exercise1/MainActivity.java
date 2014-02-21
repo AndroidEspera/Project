4404693957fb05d960916583cfca7661c43ed883
Andrew <andrew.espera@yahoo.com.ph>
2014-02-21 17:41:08 +0800

    Day3

--- src/com/addu/ece298exercise1/MainActivity.java
+++ src/com/addu/ece298exercise1/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
+test1 baiefibfei
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
